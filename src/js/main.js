209004dd76a817ee4e800b9e34af3a7b58a0f6f4
Cédric JUNG <cedced19@gmail.com>
2018-04-13 23:25:40 +0200

    Check on the fly if an input is valid

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,23 @@
 require('angular')
 angular.module('UI', [])
-.controller('UICtrl', ['$scope', function($scope) {
-
+.controller('UICtrl', ['$scope', function ($scope) {
+        $scope.points = [];
+        $scope.point = {
+            title: '',
+            coords: {
+                latitude: '',
+                longitude: ''
+            }
+        }
+        $scope.addPoint = function () {
+            $scope.points.push($scope.point)
+            $scope.point = {
+                title: '',
+                coords: {
+                    latitude: '',
+                    longitude: ''
+                }
+            }
+            console.log($scope.points);
+        }
 }])
